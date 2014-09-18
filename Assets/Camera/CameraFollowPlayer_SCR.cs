b0075c5568b8d1a20689c8a90fef4bd6ae49c2ad
RyanRothweiler <ryanrothweiler@gmail.com>
2014-09-18 16:19:44 -0700

    FirstCommit

---
+++ Assets/Camera/CameraFollowPlayer_SCR.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayer_SCR : MonoBehaviour 
+{
+    // Privates
+    private GameObject player;
+    private Vector3 playerPositionOffset;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    // Get intialize
+        player = GameObject.Find("Player");
+        playerPositionOffset = this.transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void FollowPlayer()
+    {
+        this.transform.position = player.transform.position + playerPositionOffset;
+    }
+}
